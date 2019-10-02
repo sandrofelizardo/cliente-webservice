891031e07f73124352884f7c401a6d245049b0a5
T-Gamer <T-Gamer@DESKTOP-EJ47PRO>
2019-10-02 04:49:39 -0300

    Commit inicial - Cliente Java consome WS do projeto "gerenciador1"

---
+++ cliente-webservice/src/br/com/alura/cliente/ClienteWebService.java
@@ -0,0 +1,19 @@
+package br.com.alura.cliente;
+
+import org.apache.http.client.fluent.Request;
+
+public class ClienteWebService {
+
+	public static void main(String[] args) throws Exception {
+		
+		String conteudo = Request
+			.Post("http://localhost:8080/gerenciador1/empresas")
+			.addHeader("Accept", "application/xml")
+			.execute()
+			.returnContent()
+			.asString();
+
+		System.out.println(conteudo);
+	}
+
+}
